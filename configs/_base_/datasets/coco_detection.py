b42a2d92aec7bffd5c221ec3e43980e531a7e097
Zachary <xuezihao2016@outlook.com>
2022-12-10 12:05:11 +0900

    change coco dataset

--- configs/_base_/datasets/coco_detection.py
+++ configs/_base_/datasets/coco_detection.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = 'data/'
+data_root = 'data/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
@@ -33,20 +33,17 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        classes=('person',),
-        ann_file=data_root + 'annotation_full_train.json',
-        img_prefix=data_root + 'Images/',
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        img_prefix=data_root + 'train2017/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        classes=('person',),
-        ann_file=data_root + 'annotation_full_val.json',
-        img_prefix=data_root + 'Images/',
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        classes=('person',),
-        ann_file=data_root + 'annotation_full_val.json',
-        img_prefix=data_root + 'Images/',
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
